6795f1fc7e77feaa63402a0d7e78ee4f6f78819f
SimonDdungu <dev.simonddungu@gmail.com>
2024-10-07 18:22:34 +0300

    Responsive iframe, Incomplete Responsive nav bar

---
+++ javascript/Portfolio.js
@@ -0,0 +1,12 @@
+function showSidebar(){
+    const side_bar = document.querySelector('.Mobile_Navbar');
+
+    side_bar.style.display = 'flex';
+}
+
+function hideSidebar(){
+    const side_bar = document.querySelector('.Mobile_Navbar');
+
+    side_bar.style.display = 'none';
+
+}
